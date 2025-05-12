21cb50122bcd4d0991745e511bf76bdc368bca0d
Andres <bantelis10@gmail.com>
2025-05-12 08:43:53 -0600

    Database test files, not working due to version

---
+++ MtdrSpring/backend/src/main/java/com/springboot/oracle_db_container_test/OracleDbContainerTestApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.oracle_db_container_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDbContainerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDbContainerTestApplication.class, args);
+	}
+
+}
---
+++ MtdrSpring/backend/src/test/java/com/springboot/oracle_db_container_test/OracleDbContainerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.oracle_db_container_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleDbContainerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
