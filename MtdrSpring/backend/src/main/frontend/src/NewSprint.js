730df60564e98026b90e90ed26914420363e4c1b
Andres <bantelis10@gmail.com>
2025-05-15 09:18:19 -0600

    Fixed title, removed from mytodolist

--- MtdrSpring/backend/src/main/frontend/src/NewSprint.js
+++ MtdrSpring/backend/src/main/frontend/src/NewSprint.js
@@ -563,7 +563,7 @@ function CurrentSprints() {
           fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif'
         }}
       >
-        Current Sprints :D
+        Current sprints hours Breakdown
       </Typography>
       
       <TableContainer>
