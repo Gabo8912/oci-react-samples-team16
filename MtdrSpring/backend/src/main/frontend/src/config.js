43c799c683e56e3aab40cb30c812d37007bc4f7b
Gabo8912 <128503197+Gabo8912@users.noreply.github.com>
2025-05-30 08:51:18 -0600

    Arrelgo de config 1

--- MtdrSpring/backend/src/main/frontend/src/config.js
+++ MtdrSpring/backend/src/main/frontend/src/config.js
@@ -1,7 +1,6 @@
 const config = {
-    backendUrl: "http://160.34.212.189",
-    //backendUrl: "http://localhost:8080", // alternativo local
-  };
-  
-  export default config;
-  
+    backendUrl: "http://160.34.212.189", // Asegúrate de que esta URL sea accesible
+    // backendUrl: "http://localhost:8080", // alternativo local
+};
+
+export default config;
